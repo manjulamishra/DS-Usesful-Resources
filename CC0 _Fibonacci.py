28d1f78a437ad5d55a3116390f5c3a89e3351061
Manjula Mishra <manjulamishra@users.noreply.github.com>
2019-03-24 16:13:28 -0700

    Update CC0 _Fibonacci.py

--- CC0 _Fibonacci.py
+++ CC0 _Fibonacci.py
@@ -3,8 +3,6 @@
 # 0, 1, 1, 2, 3, 5, 8, 13 . . . 
 # If n = 5, the function would return 3 because 3 is the 5th number in the Fibonacci sequence.
 
-#Manjula:
-
 def nth_fib_function(n):
   #remove the pass and put in your code
   if n == 1:
